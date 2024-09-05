b34ea49a09acf6cedac6a889086a90c09cc057bf
siljavind <silja.vj@gmail.com>
2024-09-05 13:28:00 +0200

    Initial commit

---
+++ src/main/java/dk/tec/eorzeaemporiumapi/EorzeaEmporiumApiApplication.java
@@ -0,0 +1,13 @@
+package dk.tec.eorzeaemporiumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EorzeaEmporiumApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EorzeaEmporiumApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/tec/eorzeaemporiumapi/EorzeaEmporiumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.tec.eorzeaemporiumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EorzeaEmporiumApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
